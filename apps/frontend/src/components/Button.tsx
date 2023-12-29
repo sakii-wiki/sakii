79c6f59a61a87a8ae978aef909a90a41a4c6316e
sujang958 <sujangdev@gmail.com>
2023-12-29 16:02:01 +0900

    feat: add a button component

--- apps/frontend/src/components/Button.tsx
+++ apps/frontend/src/components/Button.tsx
@@ -1,1 +1,16 @@
-const Bu
+import { ComponentProps, FC } from 'react'
+import { twMerge } from 'tailwind-merge'
+
+const Button: FC = (props: ComponentProps<'button'>) => {
+  return (
+    <button
+      {...props}
+      className={twMerge(
+        'flex flex-row items-center rounded-lg bg-neutral-100 px-4 py-2 transition duration-200 hover:bg-neutral-200',
+        props.className,
+      )}
+    ></button>
+  )
+}
+
+export default Button
