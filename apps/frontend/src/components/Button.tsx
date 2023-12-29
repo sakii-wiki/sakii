5296650368ae4ee3192fe5a629d838f3c015b74f
Songhun Cho <sujangdev@gmail.com>
2023-12-29 16:15:04 +0900

    feat(frontend): Created the document view page (#1)
    * feat: add a Next.js project
    
    * feat: add a header
    
    * feat: add a button component
    
    * feat: create the document page

---
+++ apps/frontend/src/components/Button.tsx
@@ -0,0 +1,16 @@
+import { ComponentProps, FC } from 'react'
+import { twMerge } from 'tailwind-merge'
+
+const Button: FC<ComponentProps<'button'>> = (props) => {
+  return (
+    <button
+      {...props}
+      className={twMerge(
+        'flex flex-row items-center gap-x-2 rounded-xl bg-neutral-100 px-4 py-2 font-medium transition duration-200 hover:bg-neutral-200',
+        props.className,
+      )}
+    ></button>
+  )
+}
+
+export default Button
